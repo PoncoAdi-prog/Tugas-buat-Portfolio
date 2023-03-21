292a521979ff5594f70862917c8408bd5051fd64
Ponco Adi Nugroho <81348650+PoncoAdi-prog@users.noreply.github.com>
2023-03-21 08:51:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,191 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <body>
+    <div> className="App"
+      <img src="https://gitlab.dot.co.id/uploads/-/system/appearance/header_logo/1/icon-dot.png" alt="dicoding" width="200px"></img>
+
+      <h1>Biodata dan Portfolio </h1> 
+
+      <header> className="App-header"
+
+        <figure>
+        <img src="https://pbs.twimg.com/profile_images/1566268502125998080/09nL8hfs_400x400.jpg" alt="Ponco Adi Nugroho" width="200px"></img> 
+        </figure>
+
+      <table>
+          <tr>
+          <th>Nama :</th>
+          <td>PONCO ADI NUGROHO</td>
+          </tr>
+          <tr>
+            <th>tempat lahir :</th>
+            <td>Malang,Jawa Timur</td>
+          </tr>
+          <tr>
+            <th>Agama :</th>
+            <td>Islam</td>
+          </tr>
+          <tr>
+            <th>panggilan :</th>
+            <td>Hoho</td>
+          </tr>
+          <tr>
+            <th>Email :</th>
+            <td>poncoadinugroho20@hmail.com</td>
+          </tr>
+          <tr>
+            <th>Nomor :</th>
+            <td>0822338380**</td>
+          </tr>
+        </table>
+        <br></br>
+        <br></br>
+        <h3>
+          TENTANG SAYA
+        </h3>
+        <h10>Saya Ponco Adi N lulusan Sistem Informasi 2022. Saya sedang mencari pekerjaan yang berhubungan dengan kemampuan/keterampilan 
+          atau kebutuhan perusahaan/instansi, saya ingin merasakan bekerja dengan orang hebat yang sudah ahli bersama rekan-rekan, saya akan 
+          berusaha semaksimal sebisa mungkin dengan kemampuan untuk memajukan perusahaan/instansi yang bapak/ibu pimpin
+          <br></br>
+          <br></br>
+          Semoga bapak/ibu bisa memberikan kesempatan pada saya  .
+        </h10>
+        <br></br>
+        <br></br>
+
+        <h3>
+        PENGALAMAN & MAGANG
+        </h3>
+        <h10>Percacita - Pelatihan Keterampilan (kota Malang) Juli – September 2021         
+        </h10>
+        <br></br>
+        <h10> •	Sebagai manager mencatat data-data peserta disabilitas pelatihan 
+                Membuat Kerajinan Tangan dan Rias Mata Natural. Peserta dari seluruh Indonesia.
+                <br></br>
+              •	Membantu Calon peserta mendaftarkan melalui online dan memberikan solusi/ide 
+                pada peserta.
+                <br></br>
+        </h10>
+
+        <h3>
+          PKL
+        </h3>
+           <h10>
+           AkarTuli Malang
+           </h10>
+           <br></br>
+           <h10>
+           •	Desain grafis melaksanakan tugas desain Struktur pengurus, 
+               poster ucapan dan pamflet acara. 
+           </h10>
+           <br></br>
+
+          <nav>
+           <h3>
+            Organisasi
+           </h3>
+
+            <ul>
+              <li>Gerkartin Malang</li>
+              <li>Akrtuli Malang</li>
+              <li>UKM Taekwondo</li>
+              <li>UKM Bidik (fotografis)</li>
+              <li>UKMI Al-Huda</li>
+            </ul>
+          </nav>
+        
+
+        <h3>
+          MOTTO
+        </h3>
+        <h10>Memiliki keterbatasan ini yang ada di diri kita, jangan jadikan keterbatasan 
+            sebagai suatu alasan untuk berhenti berusaha berbuat baik & berkembang. Dan 
+            jangan jadikan penghalang semangat.
+        </h10>
+        <br></br>
+
+        <h3>
+          PRESTASI
+        </h3>
+        <nav>
+          <ul>
+            <li>IPA Olimpiade Sains NASIONAL Tingkat Provinsi JATIM di Surabaya, khusus disablitas tunarungu (2015)</li>
+            <li>Juara 1 bulu tangkis putra tuna rungu dan tuna grahita usia 17 s/d 19 th. Lomba Olaharaga difabel 2016.</li>
+            <li>Juara 1 Gyeorugi Semi Prestasi Putra. Dalam Kejuaraan Taekwondo Indonesia Provinsi Jawa Timur 26-29 Desember 2019, GOR UPN Surabaya.</li>
+            <li>Sehat Bugar Disabilitas (SEGAR)</li>
+            <li>Kejuaraan Provinsi Taekwondo Indonesia Jawa Timur Virtual 2021</li>
+            <li>Piagam Penghargaan Mahasiswa Berprestasi 2019-2022</li>
+          </ul>
+          </nav>
+
+        <figure>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTexxyvX49Zzjk9tQksvPyaRPARk1GnZQ_k0A&usqp=CAU" alt="dicoding" width="200px"></img>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJmvQVNSL_vL2_Nu7hrNJXoR34hZDh79ljwQ&usqp=CAU" alt="dicoding" width="200px"></img>
+          <img src="https://1.bp.blogspot.com/-YE-Q3fzvclg/We_KMuGgcuI/AAAAAAAAGG4/VEbMZSLFErkUL6eTMs-dr4a9x1gczqC8wCLcBGAs/s1600/Bulutangkis.jpg" alt="dicoding" width="200px"></img>
+          <img src="https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/11/04083947/bukan-cuma-membakar-kalori-ini-4-manfaat-jogging-di-pagi-hari-halodoc.jpg" alt="dicoding" width="200px"></img>
+            <figcaption> Olahraga adalah hoby saya </figcaption>
+            <p>Saya suka olahraga renang , Jogging dan tekwaondo adalah hoby saya , yang penting sehat bisa mengisikan kegiatan dengan hoby.</p>
+        </figure>
+
+        <figure>
+          <img src="https://sweetrip.id/wp-content/uploads/2020/06/miejiwo_101049629_258573461864967_2414588224721876492_n.jpg" alt="dicoding" width="200px"></img>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMwyy1z_W7pQGELR3ar4k8EXZOclAN5FKCzg&usqp=CAU" alt="dicoding" width="200px"></img>
+          <figcaption>Nasi goreng istimewa dan bakso. Makan Favorit</figcaption>
+        </figure>
+
+        <p2>Minuman</p2>
+        <nav>
+          <ul>
+            <li><a href="https://i0.wp.com/img-global.cpcdn.com/recipes/ddc6ba7085d590bf/1502x1064cq70/jus-alpukat-foto-resep-utama.jpg">jus alpukay</a></li>
+            
+            <li><a href="https://i1.wp.com/www.dapurocha.com/wp-content/uploads/2018/07/Resep-Es-Manado-Minuman-Segar.jpg?resize=780%2C470&ssl=1">Es manado</a></li>
+            <li><a href="https://images.bisnis-cdn.com/posts/2021/04/06/1377388/pocari4.jpg">Pocari s</a></li>
+          </ul>
+        </nav>
+
+        <h2>Sekolah/Pendidikan</h2>
+        <nav>
+          <ol>
+            <li> <a href="https://smalbyptbmalang12.wixsite.com/smalbyptb/rumah">SD</a></li>
+            <li><a href="https://sekolah.data.kemdikbud.go.id/index.php/chome/profil/202D477E-8D18-E111-AF09-3150E5EFD560">SMP</a> </li>
+            <li><a href="https://smalbyptbmalang12.wixsite.com/smalbyptb/rumah">SMA</a></li>
+            <li><a href="https://unmer.ac.id/">Pengguruan Tinggi</a></li>
+          </ol>
+        </nav>
+
+        <main>
+          <section>
+            <article>
+              <p2>Sejarah sekolah YPTB Malang</p2>
+              <cite><a href="https://smalbyptbmalang12.wixsite.com/smalbyptb/sejarah-"> klik disini lengkap</a></cite>
+              <br></br>
+              <p2>Sejarah Unmer Malang</p2> 
+              <cite><a href="https://unmer.ac.id/sejarah-singkat/">klik disini lengkap</a></cite> 
+            </article>
+          </section>
+        </main>
+
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ====================================================================================
         </p>
+
+        <h3>Sosial Medial sosial saya ada dibawah ini.</h3>
+        <a href="https://twitter.com/poncoadinugroho"> Twitter saya</a>
+        <a href="https://id-id.facebook.com/poncoadi.nugroho.9"> Facebook</a>
+        <a href="https://www.instagram.com/ponco_adi.n20/?hl=id">Instagram</a>
+        <a href="https://www.linkedin.com/in/poncoadin20">linkedin</a>
+        <br></br>
+        <br></br>
+
+
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Ponco Adi Edit <code>src/App.js</code> and save to reload.
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,9 +195,16 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+
+      
     </div>
+
+    <footer>Ponco belajar tugas coding pakai React. Jumat,19-Maret-2023
+    </footer>
+    </body>
+    
   );
 }
 
